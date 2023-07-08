90b6c5b8e4134efa671528cd3f7e3dfe587a36eb
Adithya <bokkisum.adithya@sap.com>
2023-07-08 22:17:31 +0530

    Initialized routing by the react-routing package
    1)Made a basic navigation bar which would navigate to another page
    2)Inserted our app logo as an svg

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import CategoryMenu from "./components/category-menu/category-menu.component";
+import {Routes,Route} from "react-router-dom"
+import Home from "./Routes/home/home.component"
+import NavigationBar from "./Routes/navigation-bar/navigation-bar.component"
+
+const Shop = () => {
+  return (
+    <h1>I am Shopping</h1>
+  )
+}
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "Hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "Jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "Sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "Womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "Mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
   return (
-      <CategoryMenu categories = {categories}/>
+    <Routes>
+      <Route path="/" element = {<NavigationBar/>}>
+        <Route index element = {<Home/>}/>
+        <Route path="shop" element = {<Shop/>}/>
+      </Route>
+    </Routes>
   );
 }
 
